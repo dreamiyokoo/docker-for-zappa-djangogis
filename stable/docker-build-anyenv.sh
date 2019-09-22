7dc08d41ccbd39fb00859ab2280c41d8cb359ad0
Minoru Yokoo <yokoo@dreami.jp>
2019-09-22 13:51:48 +0900

    ecr

--- stable/docker-build-anyenv.sh
+++ stable/docker-build-anyenv.sh
@@ -1,5 +1,3 @@
 $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker build -t anyenv -f Dockerfile.anyenv  .
-docker tag anyenv:latest 487475698477.dkr.ecr.ap-northeast-1.amazonaws.com/anyenv:latest
-docker push 487475698477.dkr.ecr.ap-northeast-1.amazonaws.com/anyenv:latest
 
--- stable/docker-build-pyenv37.sh
+++ stable/docker-build-pyenv37.sh
@@ -1,5 +1,3 @@
 
 docker build -t pyenv37 -f Dockerfile.pyenv37  .
-#docker tag pyenv37:latest 487475698477.dkr.ecr.ap-northeast-1.amazonaws.com/pyenv36:latest
-#docker push 487475698477.dkr.ecr.ap-northeast-1.amazonaws.com/pyenv36:latest
 
